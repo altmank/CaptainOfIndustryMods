04b6741205287f79b0e4f962f9e2d6c2dcaf768b
PaludanC <10159539+PaludanC@users.noreply.github.com>
2022-06-06 09:56:23 +0200

    Add VehiclesCheat and the IncreaseVehicleLimit

---
+++ CheatMenu/CheatProviders/VehiclesCheatProvider.cs
@@ -0,0 +1,58 @@
+﻿using Mafi.Collections;
+using Mafi.Core.Vehicles;
+using Mafi.Core.Prototypes;
+
+namespace CaptainOfIndustryMods.CheatMenu.CheatProviders
+{
+    public class VehiclesCheatProvider : ICheatProvider
+    {
+        private readonly Mafi.Lazy<Lyst<CheatItem>> _lazyCheats;
+        private readonly ProtosDb _protosDb;
+        private readonly IVehiclesManager _vehiclesManager;
+
+
+        public VehiclesCheatProvider(VehiclesManager VehiclesManager, ProtosDb protosDb)
+        {
+            _vehiclesManager = VehiclesManager;
+            _protosDb = protosDb;
+            _lazyCheats = new Mafi.Lazy<Lyst<CheatItem>>(GetCheats);
+        }
+        public Lyst<CheatItem> Cheats => _lazyCheats.Value;
+
+        private Lyst<CheatItem> GetCheats()
+        {
+            return new Lyst<CheatItem>
+            {
+                new CheatItem
+                {
+                    Title = "Vehicle Limt add 100",
+                UsingReflection = false,
+                // Sometimes the devs make it easier than others!
+                Action = () => _vehiclesManager.IncreaseVehicleLimit(100),
+                },
+                 new CheatItem
+                {
+                    Title = "Vehicle Limt remove 100",
+                UsingReflection = false,
+                // Sometimes the devs make it easier than others!
+                Action = () => _vehiclesManager.IncreaseVehicleLimit(-100),
+                },
+                  new CheatItem
+                {
+                    Title = "Vehicle Limt add 10",
+                UsingReflection = false,
+                // Sometimes the devs make it easier than others!
+                Action = () => _vehiclesManager.IncreaseVehicleLimit(10),
+                },
+                 new CheatItem
+                {
+                    Title = "Vehicle Limt remove 10",
+                UsingReflection = false,
+                // Sometimes the devs make it easier than others!
+                Action = () => _vehiclesManager.IncreaseVehicleLimit(-10),
+                },
+            };
+
+        }
+    }
+}
